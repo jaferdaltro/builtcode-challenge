e4540659ef6c4ebcaef0ec1994e9584cded9c487
Jafer Daltro <jaferjr@gmail.com>
2021-08-21 19:48:16 -0300

    create patient

---
+++ app/models/patient.rb
@@ -0,0 +1,5 @@
+class Patient < ApplicationRecord
+  validates :name, presence: true
+  validates :birth_date, presence: true
+  validates :cpf, presence: true, uniqueness: { case_sensitive: false }
+end
---
+++ spec/factories/patients.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :patient do
+    name { Faker::Name.name  }
+    birth_date { Faker::Date.between(from: '1970-09-23', to: '2010-09-25') }
+    cpf {Faker::CPF.number }
+  end
+end
