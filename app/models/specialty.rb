c1e73b9f2038f46caebac52ab8c01242dfa9382a
Jafer Daltro <jafer.daltro@gmail.com>
2023-09-29 11:49:03 -0300

    Add specialty validation(name and description)

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,4 +1,7 @@
 class Specialty < ApplicationRecord
   has_many :doctor_specialties
   has_many :doctors, through: :doctor_specialties
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
