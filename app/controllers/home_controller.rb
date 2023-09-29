1c3f4db6d5f3d583a275f80c86872ccfa20b009c
Jafer Daltro <jafer.daltro@gmail.com>
2023-09-29 11:59:04 -0300

    Update home_controller.rb
    Add safe navigation operator

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def dashboard
-    @q = Doctor.includes(:appointments).search(params[:q])
+    @q = Doctor.includes(:appointments)&.search(params[:q])
     @pagy, @doctors = pagy(@q.result(distinct: true), items: 10)
   end
   
